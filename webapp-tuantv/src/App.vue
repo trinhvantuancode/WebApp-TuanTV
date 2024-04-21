e189bce1b069c2742d1c835bfaf65852438517e1
Tiny Sun <tuantv281099@gmail.com>
2024-04-21 15:01:47 +0700

    create custom element web component

---
+++ webapp-tuantv/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <nav>
+    
+  </nav>
+  <main>
+    <router-view />
+  </main>
+</template>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+  margin-top: 60px;
+}
+</style>
